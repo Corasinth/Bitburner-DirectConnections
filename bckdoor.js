ef53b75edf8f56f3be7086926208fa8f279f2ed1
Corasinth <107368589+Corasinth@users.noreply.github.com>
2025-03-05 04:24:00 -0600

    now parsing JSON correctly

--- bckdoor.js
+++ bckdoor.js
@@ -14,10 +14,13 @@ export async function main(ns) {
 		});
 
 	let backdoored = ns.read('backdoored_servers.txt') || ['ss']
-	if (typeof backdoored === String) {
+	
+	if (typeof backdoored === "string") {
 		backdoored = JSON.parse(backdoored)
 	}
+	
 	let serverList = JSON.parse(ns.read('server_list.txt'))
+	
 	//===============================================================================================
 
 	// Run in the terminal whatever string is passed to the function
@@ -30,7 +33,6 @@ export async function main(ns) {
 	//===============================================================================================
 
 	async function backdoor(server) {
-
 		if (ns.getServerRequiredHackingLevel(server) <= ns.getHackingLevel()) {
 			backdoored.push(server);
 			terminalCommand('backdoor');
@@ -47,11 +49,18 @@ export async function main(ns) {
 	// Runs through the servers and runs the backdoor function if the server is not in the list of servers already
 	for (let server of serverList) {
 		if (!backdoored.includes(server)) {
-			terminalCommand(`c${server}`)
+			let aliasName = ""
+			for (let char of server) {
+					if (char === '.') {
+						aliasName += 'dot'
+					} else {
+						aliasName += char
+					}
+				}
+			terminalCommand(`c${aliasName.toLowerCase()}`)
 			await backdoor(server);
 		}
+		// File to track which servers you backdoored so you don't waste time on them on the next run
+		ns.write('backdoored_servers.txt', JSON.stringify(backdoored), 'w')
 	}
-
-	// File to track which servers you backdoored so you don't waste time on them on the next run
-	ns.write('backdoored_servers.txt', JSON.stringify(backdoored), 'w')
 }
